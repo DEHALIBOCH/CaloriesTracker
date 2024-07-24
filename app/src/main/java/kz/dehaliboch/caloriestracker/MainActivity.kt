ec9b734217fe7c29e7d1e9e05f043e4121f3704e
marat <almabekov.marat@gmail.com>
2024-07-24 22:07:06 +0500

    navigation setup

--- app/src/main/java/kz/dehaliboch/caloriestracker/MainActivity.kt
+++ app/src/main/java/kz/dehaliboch/caloriestracker/MainActivity.kt
@@ -14,7 +14,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kz.dehaliboch.caloriestracker.navigation.navigate
 import kz.dehaliboch.caloriestracker.ui.theme.CaloriesTrackerTheme
+import kz.dehaliboch.core.navigation.Route
 import kz.dehaliboch.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +28,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CaloriesTrackerTheme {
-                WelcomeScreen()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Route.WELCOME) {
+                    composable(Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE) {
+
+                    }
+                    composable(Route.GENDER) {
+
+                    }
+                    composable(Route.HEIGHT) {
+
+                    }
+                    composable(Route.WEIGHT) {
+
+                    }
+                    composable(Route.NUTRIENT_GOAL) {
+
+                    }
+                    composable(Route.ACTIVITY) {
+
+                    }
+                    composable(Route.GOAL) {
+
+                    }
+                    composable(Route.TRACKER_OVERVIEW) {
+
+                    }
+                    composable(Route.SEARCH) {
+
+                    }
+                }
             }
         }
     }
